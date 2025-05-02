0f6e2d5bbb48acb86f6f923aeed3b819b9bc3380
shahman-riaz <shahmanriaz07@gmail.com>
2025-05-02 17:58:19 +0600

    projectsInfo created

--- src/constants/projectsInfo/index.ts
+++ src/constants/projectsInfo/index.ts
@@ -1,30 +1,173 @@
 export const projectsInfo = [
-    {
-        id:"project101",
-        name:"MediCommerce",
-        github:{
-            frontend:"",
-            backend:""
-        },
-        live:{
-            frontend:"",
-            backend:""
-        },
-        images:{
-            fullScreen:"",
-            TopBanner:"",
-            products:"",
-            login:"",
-            dashboard:[],
-            mobile:[]
-        },
-        technologies:["Next js","Typescript","Redux","Tailwind CSS","Node js","Express js","MongoDB","Bcrypt","JWT","Nodemailer",'Framer Motion'],
-        challenges:[
+  {
+    id: "project101",
+    name: "MediCommerce",
+    github: {
+      frontend: "https://github.com/smn-riaz/medimart-6-mediCommerce-client",
+      backend: "https://github.com/smn-riaz/medimart-6-mediCommerce-server",
+    },
+    live: {
+      frontend: "https://medicommerce-client.vercel.app",
+      backend: "https://medimart-server-three.vercel.app",
+    },
+    images: {
+      fullScreen: "https://i.ibb.co.com/LzzG4yyP/full-Screen.png",
+      TopBanner: "https://i.ibb.co.com/W4gKHDv1/topbannar.png",
+      products: "https://i.ibb.co.com/x8LhLtK9/products.png",
+      login: "https://i.ibb.co.com/zHJ2ZYQL/register.png",
 
-        ],
-        futurePlans:[
+      dashboard: [
+        {
+          title: "",
+          img: "https://i.ibb.co.com/R4p5D8jH/createproduct.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/bj27DNyT/orders.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/39KW8Q9r/dashboard.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/rGKKyn3x/all-medicines.png",
+          features: [""],
+        },
+      ],
+      mobile: ["https://i.ibb.co.com/8DMvntNK/mobile.png"],
+    },
+    technologies: [
+      "Next js",
+      "Typescript",
+      "Redux",
+      "Tailwind CSS",
+      "Node js",
+      "Express js",
+      "MongoDB",
+      "Bcrypt",
+      "JWT",
+      "Nodemailer",
+      "Framer Motion",
+    ],
+    challenges: [],
+    futurePlans: [],
+  },
+  {
+    id: "project102",
+    name: "CycleHouse",
+    github: {
+      frontend: "https://github.com/smn-riaz/medimart-6-mediCommerce-client",
+      backend: "https://github.com/smn-riaz/medimart-6-mediCommerce-server",
+    },
+    live: {
+      frontend: "https://medicommerce-client.vercel.app",
+      backend: "https://medimart-server-three.vercel.app",
+    },
+    images: {
+      fullScreen: "https://i.ibb.co.com/LzzG4yyP/full-Screen.png",
+      TopBanner: "https://i.ibb.co.com/W4gKHDv1/topbannar.png",
+      products: "https://i.ibb.co.com/x8LhLtK9/products.png",
+      login: "https://i.ibb.co.com/zHJ2ZYQL/register.png",
 
-        ]
+      dashboard: [
+        {
+          title: "",
+          img: "https://i.ibb.co.com/R4p5D8jH/createproduct.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/bj27DNyT/orders.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/39KW8Q9r/dashboard.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/rGKKyn3x/all-medicines.png",
+          features: [""],
+        },
+      ],
+      mobile: ["https://i.ibb.co.com/8DMvntNK/mobile.png"],
+    },
+    technologies: [
+      "Next js",
+      "Typescript",
+      "Redux",
+      "Tailwind CSS",
+      "Node js",
+      "Express js",
+      "MongoDB",
+      "Bcrypt",
+      "JWT",
+      "Nodemailer",
+      "Framer Motion",
+    ],
+    challenges: [],
+    futurePlans: [],
+  },
+  {
+    id: "project101",
+    name: "MediCommerce",
+    github: {
+      frontend: "https://github.com/smn-riaz/medimart-6-mediCommerce-client",
+      backend: "https://github.com/smn-riaz/medimart-6-mediCommerce-server",
+    },
+    live: {
+      frontend: "https://medicommerce-client.vercel.app",
+      backend: "https://medimart-server-three.vercel.app",
+    },
+    images: {
+      fullScreen: "https://i.ibb.co.com/LzzG4yyP/full-Screen.png",
+      TopBanner: "https://i.ibb.co.com/W4gKHDv1/topbannar.png",
+      products: "https://i.ibb.co.com/x8LhLtK9/products.png",
+      login: "https://i.ibb.co.com/zHJ2ZYQL/register.png",
 
-    }
-]
+      dashboard: [
+        {
+          title: "",
+          img: "https://i.ibb.co.com/R4p5D8jH/createproduct.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/bj27DNyT/orders.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/39KW8Q9r/dashboard.png",
+          features: [""],
+        },
+        {
+          title: "",
+          img: "https://i.ibb.co.com/rGKKyn3x/all-medicines.png",
+          features: [""],
+        },
+      ],
+      mobile: ["https://i.ibb.co.com/8DMvntNK/mobile.png"],
+    },
+    technologies: [
+      "Next js",
+      "Typescript",
+      "Redux",
+      "Tailwind CSS",
+      "Node js",
+      "Express js",
+      "MongoDB",
+      "Bcrypt",
+      "JWT",
+      "Nodemailer",
+      "Framer Motion",
+    ],
+    challenges: [],
+    futurePlans: [],
+  },
+];
